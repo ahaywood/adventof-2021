21c9d9fc01b81e6bc82c51a075996c8648bb6455
avneesh0612 <avneeshagarwal0612@gmail.com>
2021-12-02 19:44:30 +0530

    added meta tags for advent of css

--- adventofcss/src/routes/__layout.svelte
+++ adventofcss/src/routes/__layout.svelte
@@ -1,5 +1,24 @@
 <slot><!-- optional fallback --></slot>
 
+<svelte:head>
+	<title>Advent of CSS</title>
+	<link rel="icon" href="/images/icon-192x192.png" />
+	<link rel="manifest" href="/manifest.json" />
+	<link rel="apple-touch-icon" href="/images/icon-192x192.png" />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:site" content="@selfteachme" />
+	<meta name="twitter:creator" content="@selfteachme" />
+	<meta property="og:url" content="https://www.adventofcss.com/" />
+	<meta property="og:title" content="Advent of CSS" />
+	<meta property="og:description" content="24 CSS Challenges for the Season" />
+	<meta property="og:image" content="/images/Banner.png" />
+	<meta property="og:image:alt" content="Advent of CSS" />
+	<meta property="og:image:width" content="512" />
+	<meta property="og:image:height" content="512" />
+	<meta property="og:site_name" content="Advent of CSS" />
+	<link rel="canonical" href="https://www.adventofcss.com/" />
+</svelte:head>
+
 <style lang="scss">
 	@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,700;1,400;1,700&display=swap');
 
@@ -45,5 +64,7 @@
 		background: var(--ceramic) url('/images/snow.svg') center top repeat;
 		margin: 0;
 		padding: 0;
+		width: 100vw;
+		overflow-x: hidden;
 	}
 </style>
