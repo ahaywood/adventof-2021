4c80ce17cc3ec6d6da6f9c6adfa6fa3b39aaedc9
Amy Dutton <amy@ahhacreative.com>
2022-12-01 23:24:15 -0600

    ✨ Add unsubscribe pages

---
+++ adventofjs/src/routes/bundle-unsubscribe.svelte
@@ -0,0 +1,19 @@
+<div class="wrapper">
+	<h1>Sorry to see you go.</h1>
+</div>
+
+<style lang="scss">
+	.wrapper {
+		align-items: center;
+		display: flex;
+		height: 100vh;
+		justify-content: center;
+		width: 100vw;
+	}
+
+	h1 {
+		font-family: var(--silverAge);
+		font-size: 3rem;
+		color: white;
+	}
+</style>
